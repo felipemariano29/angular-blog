9173f3aa8514ec2c3e971c1c8c5683c7c2dd778f
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2023-06-01 10:53:34 -0300

    content created

--- src/app/mock/cards.mock.ts
+++ src/app/mock/cards.mock.ts
@@ -3,30 +3,34 @@ import { CardInterface } from '../interfaces/card.interface';
 export const CardsMock: CardInterface[] = [
   {
     id: 1,
-    image: 'https://placehold.co/600x400',
-    title: 'Lorem, ipsum dolor.',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Saint_Jerome_Writing-Caravaggio_%281605-6%29.jpg/1280px-Saint_Jerome_Writing-Caravaggio_%281605-6%29.jpg',
+    title: 'Saint Jerome Writing',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore error doloremque ex numquam, in quas incidunt similique praesentium exercitationem eum.',
+      'Also called Saint Jerome in His Study or simply Saint Jerome, is an oil painting by Italian painter Caravaggio. Generally dated to 1605–06, the painting is located in the Galleria Borghese in Rome.',
   },
   {
     id: 2,
-    image: 'https://placehold.co/600x400',
-    title: 'Lorem, ipsum 1.',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Spas_vsederzhitel_sinay.jpg/800px-Spas_vsederzhitel_sinay.jpg',
+    title: 'Pantocrator ',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore error doloremque ex numquam, in quas incidunt similique praesentium exercitationem eum.',
+      'Refere-se a uma forma de representação de Jesus, que é retratado com a mão direita, em posição de bênção — com o polegar voltado para si, os dedos médio e indicador em posição oblíqua, quase vertical, e os outros dedos dobrados em direção à palma da mão. Essa posição da mão direita, com dois dedos erguidos, indica a sua dupla natureza (divina e humana), enquanto a sua participação na Trindade, como segunda Pessoa, é indicada pelos três dedos unidos nas pontas; na mão esquerda, estão as Sagradas Escrituras.',
   },
   {
     id: 3,
-    image: 'https://placehold.co/600x400',
-    title: 'Lorem, ipsum 2.',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Leonardo_da_Vinci_%281452-1519%29_-_The_Last_Supper_%281495-1498%29.jpg/1920px-Leonardo_da_Vinci_%281452-1519%29_-_The_Last_Supper_%281495-1498%29.jpg',
+    title: 'A Última Ceia',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore error doloremque ex numquam, in quas incidunt similique praesentium exercitationem eum.',
+      'É um afresco de Leonardo da Vinci para a igreja de Santa Maria delle Grazie em Milão, Itália. O trabalho presume-se que tenha sido iniciado por volta de 1495-96 e foi encomendado como parte de um plano de reformas na igreja e nos seus edifícios conventuais pelo patrono de Leonardo, Ludovico Sforza, duque de Milão. Representa o episódio bíblico da Última Ceia de Jesus com os Apóstolos antes de ser preso e crucificado. É um dos bens culturais mais conhecidos e estimados do mundo.',
   },
   {
     id: 4,
-    image: 'https://placehold.co/600x400',
-    title: 'Lorem, ipsum 3.',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b0/Rembrandt_-_Aristotle_with_a_Bust_of_Homer_-_WGA19232.jpg',
+    title: 'Aristotle with a Bust of Homer',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore error doloremque ex numquam, in quas incidunt similique praesentium exercitationem eum.',
+      "Also known as Aristotle Contemplating a Bust of Homer, is an oil-on-canvas painting by Rembrandt that depicts Aristotle wearing a gold chain and contemplating a sculpted bust of Homer. It was created as a commission for Don Antonio Ruffo's collection. It was bought and sold by several collectors until it was eventually purchased by the Metropolitan Museum of Art. The mysterious tone in the painting has led several scholars to different interpretations of Rembrandt's theme.",
   },
 ];
